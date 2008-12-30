4264d18689db716e7c8276cd0916b12b2dab5ac1
Jeffrey Sharkey <jeff@jsharkey.org>
2008-12-30 18:45:14 +0000

    forgot a few new files  :)

---
+++ src/org/tunescontrol/PrefsActivity.java
@@ -0,0 +1,15 @@
+package org.tunescontrol;
+
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+
+public class PrefsActivity extends PreferenceActivity {
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		addPreferencesFromResource(R.xml.preferences);
+	
+	}
+
+}
